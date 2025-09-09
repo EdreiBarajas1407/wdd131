8377759f613bf55d5e067724431b1d0d5718202d
Edrei Barajas <edrei.barajas14@gmail.com>
2025-09-08 19:03:41 -0600

    Completing Bom activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,9 +2,28 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 const list = document.querySelector('.list');
 
-const li = document.queryElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+button.addEventListener('click', () => {
+    if (input.value !== '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+        this.list.appendChild(li);
+
+        deleteButton.addEventListener('click', () => {
+            this.list.removeChild(li);
+            input.focus();
+        });
+
+        input.focus();
+
+        input.value = '';
+    }
+    else {
+        input.focus();
+        input.value = 'Insert something';
+    }
+});
